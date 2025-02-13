cb267fedbddaa4868692ffb69f88ec0fbdaa413c
dumbass-ux <198928784+dumbass-ux@users.noreply.github.com>
2025-02-13 13:03:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-function showSection(section) {
+ function showSection(section) {
     document.getElementById('game-section').style.display = 'none';
     document.getElementById('quiz-section').style.display = 'none';
     document.getElementById('final-section').style.display = 'none';
 
     document.getElementById(section).style.opacity = 0;
     document.getElementById(section).style.display = 'block';
-    
+
     setTimeout(() => {
         document.getElementById(section).style.opacity = 1;
     }, 200); // Smooth fade-in effect
-}
+    }
 }
 function showSticker(answer) {
     let sticker = document.createElement("img");
